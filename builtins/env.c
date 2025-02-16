1df2d78c84d4b4f3ef9bbf2f1feff06f9a4e1033
Tameem Mahmoud <tmahmoud@lab2r3s1.42abudhabi.ae>
2025-02-16 16:40:38 +0400

    get pull

--- builtins/env.c
+++ builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: tmahmoud <tmahmoud@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 15:33:53 by aal-hawa          #+#    #+#             */
-/*   Updated: 2025/02/15 17:35:40 by tmahmoud         ###   ########.fr       */
+/*   Updated: 2025/02/16 14:21:38 by tmahmoud         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,26 +40,26 @@ Continue work
 int funcName (char *str, char c, char **before_sign, char **after_sign)
 */
 
-char **split_export_input()
-{
+// char **split_export_input()
+// {
 	
-}
+// }
 
-char **split_env_arg(char *str, char c)
-{
-	char **result;
-	char *str_after_sign;
-	int i;
+// char **split_env_arg(char *str, char c)
+// {
+// 	char **result;
+// 	char *str_after_sign;
+// 	int i;
 
-	i = 1;
-	str_after_sign = NULL;
-	result = ft_split(str, c);
-	if(result[2] != "\0")
-	while (result[i])
-	{
-		str_after_sign = ft_strjoin(str_after_sign, result[i]);
-		free(result[i]);
-		result[i++] = NULL;
-	}
-	return (result);
-}
+// 	i = 1;
+// 	str_after_sign = NULL;
+// 	result = ft_split(str, c);
+// 	if(result[2] != "\0")
+// 	while (result[i])
+// 	{
+// 		str_after_sign = ft_strjoin(str_after_sign, result[i]);
+// 		free(result[i]);
+// 		result[i++] = NULL;
+// 	}
+// 	return (result);
+// }
